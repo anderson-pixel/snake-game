6e5aa179e06d0d2542fc506008ee71573cfb21f2
anderson <anderson.ap133@gmail.com>
2022-06-11 16:00:54 -0300

    Created Snake Game Canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import './snake'
 
 function App() {
   return (
     <div className="App">
+    <h1 className="">Snake Game</h1>
+    <canvas id='game' width='400' height='400'></canvas>
     </div>
   );
 }
---
+++ src/snake.tsx
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('game') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+let speed = 5;
+
+function drawGame(){
+
+}
+
+
+export {}
