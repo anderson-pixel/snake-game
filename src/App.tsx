069a453d073b32c3b7d8eddd98eecbb4d48fa221
anderson <anderson.ap133@gmail.com>
2022-06-11 19:59:40 -0300

    Created Game Background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './snake'
 
 function App() {
   return (
@@ -9,5 +8,24 @@ function App() {
     </div>
   );
 }
+function loaded() {
+  const canvas =  document.getElementById('game') as HTMLCanvasElement;
+  const ctx = canvas.getContext('2d');
+  
+  function clearScreen(){
+    ctx!.fillStyle = 'green';
+    ctx?.fillRect(0,0,canvas.width,canvas.height);
+}
+}
+
+  
+  let speed = 7;
+  
+  function drawGame(){
+      console.log('drawgame')
+      setTimeout(drawGame, 1000/ speed)
+  }
+  
+drawGame();
 
 export default App;
