82aa71f3672568b0147c2c24256e5e4ce5b53a85
anderson <anderson.ap133@gmail.com>
2022-06-15 13:27:58 -0300

    Added Restart Button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
     <h1 className="">Snake Game</h1>
-    <canvas id='game' width='420' height='420'></canvas>
+    <button onClick={() => window.location.reload()}>Restart</button> .
+    <canvas id='game' width='400' height='400'></canvas>
     </div>
   );
 }
